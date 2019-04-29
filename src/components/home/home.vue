2055d6fe3219bf6d638f3c78eaedcc6bd4ab6365
18203641939@163.com <18203641939@163>
2019-04-29 19:19:54 +0800

    19:19

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,15 +1,22 @@
 <template >
   <div class="home">
     <!-- 头部轮播 -->
+    <hsearch></hsearch>
     <hBanner></hBanner>
+    <hCate></hCate>
     <div class="aaa"></div>
   </div>
 </template>
 
 <script>
 import hBanner from './components/hBanner'
+import hsearch from './components/hsearch'
+import hCate from './components/hCate'
 export default {
   components: {
+    hBanner,
+    hsearch,
+    hCate
   }
 }
 </script>
