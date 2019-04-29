628aba1668cf750af6b71863f1cff23f7608843a
Renruizhen <2423155715@qq.com>
2019-04-29 15:02:25 +0800

    bnav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <bnav></bnav>
     <router-view></router-view>
   </div>
 </template>
@@ -7,14 +8,14 @@
 <script>
 // 初始样式
 import '@/gcss/reset.css'
-
+import '@/gcss/fonts.css'
+import bnav from '@/common/bnav.vue'
 export default {
   name: 'App',
   components: {
-
+    bnav
   }
 }
-
 </script>
 
 <style>
