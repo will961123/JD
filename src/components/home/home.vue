e6759686bbfefc1694995f01b9a93bc9ac012bea
Yue <1978063895@qq.com>
2019-04-29 16:56:27 +0800

    hCate

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,35 +1,18 @@
 <template >
   <div class="home">
-<<<<<<< HEAD
-    <hCate/>
-
-=======
     <!-- 头部轮播 -->
     <hBanner></hBanner>
     <div class="aaa"></div>
->>>>>>> b3a05482d7a8511b68c3880c2528cc841cd8ef9f
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import hCate from '../home/components/hCate'
 export default {
   components: {
-    hCate
-=======
-import hBanner from './components/hBanner'
-export default {
-  components: {
->>>>>>> b3a05482d7a8511b68c3880c2528cc841cd8ef9f
   }
 }
 </script>
 
-<<<<<<< HEAD
-<style scoped>
-=======
 <style scoped lang='less'>
->>>>>>> b3a05482d7a8511b68c3880c2528cc841cd8ef9f
 
 </style>
