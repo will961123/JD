b32b9bd16def3d762b0148b9403e52fccc5bc675
will1123 <will1123@fox.com>
2019-04-29 16:07:50 +0800

    '轮播图'

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,16 +1,20 @@
 <template >
   <div class="home">
-    home
+    <!-- 头部轮播 -->
+    <hBanner></hBanner>
+    <div class="aaa"></div>
   </div>
 </template>
 
 <script>
-
+import hBanner from './components/hBanner'
 export default {
-
+  components: {
+    hBanner
+  }
 }
 </script>
 
-<style scope>
+<style scoped lang='less'>
 
 </style>
