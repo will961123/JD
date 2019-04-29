c4a33adce85c8818b45a124168b8190530aebe95
will1123 <will1123@fox.com>
2019-04-29 20:26:13 +0800

    修改eslint

--- src/components/home/components/hBanner.vue
+++ src/components/home/components/hBanner.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import {gethBanner} from '@/api'
+import { gethBanner } from '@/api'
 export default {
   name: 'carrousel',
   data () {
--- src/components/home/components/jdspike.vue
+++ src/components/home/components/jdspike.vue
@@ -38,7 +38,6 @@
               <li>
                 <span>{{leftTime.second}}</span>
                 <i></i>
-
               </li>
             </ul>
           </div>
