94c3f72eb86119b90732aa38d6d1e7f5b9aa7e5e
will1123 <will1123@fox.com>
2019-04-30 14:22:38 +0800

    右边

--- src/components/category/category.vue
+++ src/components/category/category.vue
@@ -1,24 +1,34 @@
 <template>
-    <div class="category">
-        <catesearch></catesearch>
-        <!-- 左边列表栏部分 -->
-        <cateleft></cateleft>
+  <div class="category">
+    <catesearch></catesearch>
+    <!-- 左边列表栏部分 -->
+    <cateleft></cateleft>
+    <div class="rightbox">
+      <cateright></cateright>
     </div>
+  </div>
 </template>
 
 <script>
 import catesearch from './components/Catesearch'
 import cateleft from './components/cateleft'
+import cateright from './components/cateRight'
 export default {
   components: {
     catesearch,
-    cateleft
+    cateleft,
+    cateright
   }
 }
 </script>
 
 <style lang="less" scoped>
-.category{
-  height:100%
+.category {
+  height: 100%;
+  width: 100%;
+  .rightbox {
+    width: calc(100% - 170px);
+    overflow: hidden;
+  }
 }
 </style>
