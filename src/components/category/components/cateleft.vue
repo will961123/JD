0194e42c539afd6e639448d32d8034e4372bae5c
yuan <18203641939@163.com>
2019-05-05 20:32:14 +0800

    fafasf

--- src/components/category/components/cateleft.vue
+++ src/components/category/components/cateleft.vue
@@ -33,6 +33,7 @@ export default {
       /* 如果点击的索引就是this.currentIndex 停止滚动 */
       if (index === this.currentIndex) {
         console.log('是当前索引值 停止滚动')
+        
         return
       }
       /* 判断是否滚动 总长度-滚动上去的高度=设备高度 停止 */
@@ -41,6 +42,7 @@ export default {
         this.$refs.mytop.offsetHeight
       ) {
         console.log(this.$refs.myul.offsetHeight, this.$refs.mytop.scrollTop)
+        this.$refs.myli.className = 'active'
         return
       }
 
