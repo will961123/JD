fd8a1ecfd4918ff01b7003d2cbc38706a7271d7b
dongdeqian <1131590828.com>
2019-05-06 15:47:21 +0800

    ddq-userinfo

--- src/components/my/my.vue
+++ src/components/my/my.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="my">
     <mHeader></mHeader>
+    <userInfo></userInfo>
   </div>
 </template>
 
 <script>
 import mHeader from './components/mHeader'
+import userInfo from './components/userInfo'
 export default {
   components: {
-    mHeader
+    mHeader,
+    userInfo
   }
 }
 </script>
