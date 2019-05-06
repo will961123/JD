8704fe796cd8ca8b3f74248aa6d5c5e15999dfe7
Yue <1978063895@qq.com>
2019-05-06 14:00:13 +0800

    修改eslint报错

--- src/components/my/my.vue
+++ src/components/my/my.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <Login/>
 </template>
 
@@ -8,18 +7,6 @@ import Login from './components/login'
 export default {
   components: {
     Login
-=======
-  <div class="my">
-    <mHeader></mHeader>
-  </div>
-</template>
-
-<script>
-import mHeader from './components/mHeader'
-export default {
-  components: {
-    mHeader
->>>>>>> 6a3a664b28210abb924a552076c409fbf0c3d847
   }
 }
 </script>
